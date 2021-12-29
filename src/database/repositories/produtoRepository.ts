c2ca50dbbf4ead0e8fc6579e16af357422605ad1
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2021-12-29 08:21:10 -0300

    adicionado imagemUrl

--- src/database/repositories/produtoRepository.ts
+++ src/database/repositories/produtoRepository.ts
@@ -155,6 +155,7 @@ class ProdutoRepository {
           'precoOferta',
           'importHash',
           'status',
+          'imagemUrl',
           'imagemPromocional',
           'promocaoEncerramento',
           'promocaoCriacao',
