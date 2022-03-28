c1411cb93ef8e9f2e63d6e590263a2968f4151b2
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-03-28 14:49:02 -0300

    teste no server

--- src/api/pedido/pedidoImport.ts
+++ src/api/pedido/pedidoImport.ts
@@ -2,22 +2,44 @@ import PermissionChecker from '../../services/user/permissionChecker';
 import ApiResponseHandler from '../apiResponseHandler';
 import Permissions from '../../security/permissions';
 import PedidoService from '../../services/pedidoService';
+import pagarme from 'pagarme'
 
 export default async (req, res, next) => {
   try {
-    new PermissionChecker(req).validateHas(
-      Permissions.values.pedidoImport,
-    );
 
-    await new PedidoService(req).import(
-      req.body.data,
-      req.body.importHash,
-    );
+   let sk = "sk_test_KEB42ybcqCNDv1Xq"
 
-    const payload = true;
+    // const lib = require('../../../pagarme-core-api-nodejs/lib');
 
-    await ApiResponseHandler.success(req, res, payload);
-  } catch (error) {
+    // // Configuration parameters and credentials
+    // lib.Configuration.basicAuthUserName = "sk_test_KEB42ybcqCNDv1Xq"; // The username to use with basic authentication
+    // lib.Configuration.basicAuthPassword = "";
+
+    // console.log("lib.Configuration.basicAuthUserName")
+    // console.log(lib.Configuration.basicAuthUserName)
+    // console.log("lib.Configuration.basicAuthPassword")
+    // console.log(lib.Configuration.basicAuthPassword)
+
+    var fs = require('fs');
+    const request = require("request");
+    var body =  { User: sk, Password: ''  };
+
+    var options = {                 
+        method: 'POST',             
+        uri: 'https://api.pagar.me/core/v5/orders',                    
+        headers: {               
+        'Authorization': 'Basic ' + Buffer.from(`${sk}`).toString('base64'),
+        'Content-Type': 'application/json'              
+        },
+        json : ''
+    };    
+
+request(options, function(error, response, body) {  
+    console.log(response.body);
+});
+
+    await ApiResponseHandler.success(req, res, ' payload');
+} catch (error) {
     await ApiResponseHandler.error(req, res, error);
   }
 };
